499c35e6677666f0d285a96b13e297c4519861f9
kyo <phongbinh2511@gmail.com>
2021-04-08 20:01:24 +0800

    [master] Made Base64 encode more decipherable.

--- Example02(改玩了)/script.js
+++ Example02(改玩了)/script.js
@@ -21,7 +21,7 @@ window.addEventListener('load', function() {
 });
 
 function loadData(fileText, treemapContainer) {
-    var csvUri = 'data:text/plain;base64,' + Base64.encode(fileText);
+    var csvUri = 'data:text/plain;base64,' + get_base64_encode(fileText);
     d3.csv(csvUri, function(rows) { // update global variable `csvData`
         var jobData = rows.map(function(row) { // process salary data
             var salaryText = row['待遇'].replace(/,/g, ''); // remove all commas
@@ -45,33 +45,31 @@ function loadData(fileText, treemapContainer) {
     });
 }
 
-var Base64 = {
-    _keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
-    encode: function(r) {
-        var t, e, o, a, h, c, n = "",
-            d = 0;
-        for (r = Base64._utf8_encode(r); d < r.length;) o = (c = r.charCodeAt(d++)) >> 2, a = (3 & c) << 4 | (t = r.charCodeAt(d++)) >> 4, h = (15 & t) << 2 | (e = r.charCodeAt(d++)) >> 6, c = 63 & e, isNaN(t) ? h = c = 64 : isNaN(e) && (c = 64), n = n + this._keyStr.charAt(o) + this._keyStr.charAt(a) + this._keyStr.charAt(h) + this._keyStr.charAt(c);
-        return n
-    },
-    decode: function(r) {
-        var t, e, o, a, h, c = "",
-            n = 0;
-        for (r = r.replace(/[^A-Za-z0-9\+\/\=]/g, ""); n < r.length;) t = this._keyStr.indexOf(r.charAt(n++)) << 2 | (o = this._keyStr.indexOf(r.charAt(n++))) >> 4, e = (15 & o) << 4 | (a = this._keyStr.indexOf(r.charAt(n++))) >> 2, o = (3 & a) << 6 | (h = this._keyStr.indexOf(r.charAt(n++))), c += String.fromCharCode(t), 64 != a && (c += String.fromCharCode(e)), 64 != h && (c += String.fromCharCode(o));
-        return c = Base64._utf8_decode(c)
-    },
-    _utf8_encode: function(r) {
-        r = r.replace(/\r\n/g, "\n");
-        for (var t = "", e = 0; e < r.length; e++) {
-            var o = r.charCodeAt(e);
-            o < 128 ? t += String.fromCharCode(o) : (127 < o && o < 2048 ? t += String.fromCharCode(o >> 6 | 192) : (t += String.fromCharCode(o >> 12 | 224), t += String.fromCharCode(o >> 6 & 63 | 128)), t += String.fromCharCode(63 & o | 128))
-        }
-        return t
-    },
-    _utf8_decode: function(r) {
-        for (var t = "", e = 0, o = c1 = c2 = 0; e < r.length;)(o = r.charCodeAt(e)) < 128 ? (t += String.fromCharCode(o), e++) : 191 < o && o < 224 ? (c2 = r.charCodeAt(e + 1), t += String.fromCharCode((31 & o) << 6 | 63 & c2), e += 2) : (c2 = r.charCodeAt(e + 1), c3 = r.charCodeAt(e + 2), t += String.fromCharCode((15 & o) << 12 | (63 & c2) << 6 | 63 & c3), e += 3);
-        return t
+function get_base64_encode(text) {
+    const KEY_STRING = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+    var t, e, o, a, h, c;
+    var result = '';
+    var d = 0;
+    for (text = get_utf8_encode(text); d < text.length;) {
+        o = (c = text.charCodeAt(d++)) >> 2;
+        a = (3 & c) << 4 | (t = text.charCodeAt(d++)) >> 4;
+        h = (15 & t) << 2 | (e = text.charCodeAt(d++)) >> 6;
+        c = 63 & e;
+        isNaN(t) ? h = c = 64 : isNaN(e) && (c = 64);
+        result = result + KEY_STRING.charAt(o) + KEY_STRING.charAt(a) + KEY_STRING.charAt(h) + KEY_STRING.charAt(c);
     }
-};
+    return result;
+}
+
+function get_utf8_encode(text) {
+    text = text.replace(/\r\n/g, '\n');
+    var result = '';
+    for (var i = 0; i < text.length; i++) {
+        var o = text.charCodeAt(i);
+        o < 128 ? result += String.fromCharCode(o) : (127 < o && o < 2048 ? result += String.fromCharCode(o >> 6 | 192) : (result += String.fromCharCode(o >> 12 | 224), result += String.fromCharCode(o >> 6 & 63 | 128)), result += String.fromCharCode(63 & o | 128));
+    }
+    return result;
+}
 
 function getAllBoss(jobData) {
     var allBoss = [];
