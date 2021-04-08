5615d7a9f47dc72a9189b29582de21d3389f69bf
kyo <phongbinh2511@gmail.com>
2021-04-08 20:24:35 +0800

    [master] Formatted a function.

--- Example02(改玩了)/script.js
+++ Example02(改玩了)/script.js
@@ -110,29 +110,29 @@ function getD3Json(jobData, allBoss, mapping) {
 }
 
 function getSalaryScoreMap(jobData, salaryScoreMapInterval) {
-    var flatten = []
+    var flatten = [];
 
-    var correspondingMapping = {}
+    var correspondingMapping = {};
 
     //將所有數值取出置入faltten中
     for (var i = 0; i < jobData.length; i++) {
         for (var j in jobData[i]) {
             if (typeof jobData[i][j] == "number") {
-                correspondingMapping[jobData[i][j]] = null
-                flatten.push(jobData[i][j])
+                correspondingMapping[jobData[i][j]] = null;
+                flatten.push(jobData[i][j]);
             }
         }
     }
 
     flatten.sort();
-        //數值映射公式
-        //假設原本的區間為Omin~Omax，對應的區間為Nmin~Nmax
-        //公式為Nmapping = [(Nmax-Nmin) / (Omax-Omin) * (O-Omin)] + Nmin
+    //數值映射公式
+    //假設原本的區間為Omin~Omax，對應的區間為Nmin~Nmax
+    //公式為Nmapping = [(Nmax-Nmin) / (Omax-Omin) * (O-Omin)] + Nmin
     for (var k in correspondingMapping) {
-        correspondingMapping[k] = ((salaryScoreMapInterval[1] - salaryScoreMapInterval[0]) / (flatten[flatten.length - 1] - flatten[0])) * (parseFloat(k) - flatten[0]) + salaryScoreMapInterval[0]
+        correspondingMapping[k] = ((salaryScoreMapInterval[1] - salaryScoreMapInterval[0]) / (flatten[flatten.length - 1] - flatten[0])) * (parseFloat(k) - flatten[0]) + salaryScoreMapInterval[0];
     }
 
-    return correspondingMapping
+    return correspondingMapping;
 }
 
 //產生SVG
