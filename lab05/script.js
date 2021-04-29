c68490b087f28580b9703586f6a99fc060e8760a
kyo <phongbinh2511@gmail.com>
2021-04-29 18:22:26 +0800

    [master] Final update lab05.

--- lab05/script.js
+++ lab05/script.js
@@ -1,14 +1,28 @@
-d3.text('data.csv').then(function(data) {
-    var parsedCSV = d3.csvParseRows(data);
-    d3.select('body')
-      .selectAll('div')
-      .data(parsedCSV)
-      .enter()
-      .filter(function(d, i) { return(i != 0); }) // ignore first row
-      .append('div')
-      .attr('class', 'bar')
-      .text(function(d) { return d[1]; })
-      .style('height', function(d) {
-          return(d[1] * 20 + 'px');
-      });
+const MAX_FREQUENCY_VALUE = 39;
+const BAR_HEIGHT_SCALE = 15;
+
+d3.text('data.csv').then(function(csvText) {
+    var rows = d3.csvParseRows(csvText);
+    var frequencyDiv = d3.select('body')
+        .selectAll('div')
+        .data(rows)
+        .enter()
+        .filter(function(row, index) {
+            return index != 0;
+        }) // ignore first row
+        .append('div')
+        .attr('class', 'frequency');
+    frequencyDiv.append('div')
+        .attr('class', 'frequency-value')
+        .text(function(row) {
+            return row[1] === '0' ? '' : row[1];
+        });
+    frequencyDiv.append('div')
+        .attr('class', 'bar')
+        .style('height', function(row) {
+            return row[1] * BAR_HEIGHT_SCALE + 'px';
+        })
+        .style('background-color', function(row) {
+            return 'rgba(255, 0, 0, ' + (row[1] / MAX_FREQUENCY_VALUE) + ')';
+        });
 });
